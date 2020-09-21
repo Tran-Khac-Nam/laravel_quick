febf64683d8b9bded14d6f19c6c7a2709bc41a99
tran khac nam <nam3061998@gmail.com>
2020-09-21 08:39:38 +0700

    cat giao dien cho quicktask

---
+++ resources/views/layout/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<title>Bootstrap CRUD Data Table for Database with Modal Form</title>
+<base href="  {{asset('') }}">
+<link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="bower_components/google-material-icons/google-material-icons.css">
+<link rel="stylesheet" href="{{ asset(mix('/css/index.css')) }}">
+<script src="bower_components/jquery/dist/jquery.min.js"></script>
+<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+@include('layout.script')
+
+</head>
+<body>
+		@yield('content')
+
+</body>
+</html>
